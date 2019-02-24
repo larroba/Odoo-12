3ed7489f9ba8f2909425be5f92aa3ec1edc98ede
larroba <todaherp@gmail.com>
2019-02-24 14:58:40 -0500

    .

---
+++ library/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+        Prueba""",
+
+    'description': "Library Management",
+
+    'author': "TODAH ERP",
+    'website': "http://www.todah.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # ' views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'installable': True,
+}
+
